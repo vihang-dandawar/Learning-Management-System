836dcb95f19024900b4c8906f3602d07e682545c
Vihang <vihangreddy1234@gmail.com>
2025-08-13 03:03:30 +0530

    jdbc adjust

--- EazySchool_FullStack_Backend/src/main/java/com/vikki/vikkiSchool/config/SecurityConfig.java
+++ EazySchool_FullStack_Backend/src/main/java/com/vikki/vikkiSchool/config/SecurityConfig.java
@@ -109,7 +109,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://vikkischool-lms.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);

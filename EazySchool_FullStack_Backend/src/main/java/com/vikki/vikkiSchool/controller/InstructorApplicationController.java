d3468738956f17f72523014e9bd2b7e200a84d6e
Vihang <vihangreddy1234@gmail.com>
2025-08-13 11:46:13 +0530

    changed deployement config

--- EazySchool_FullStack_Backend/src/main/java/com/vikki/vikkiSchool/controller/InstructorApplicationController.java
+++ EazySchool_FullStack_Backend/src/main/java/com/vikki/vikkiSchool/controller/InstructorApplicationController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 
-
+@RequestMapping("/api/instructor-applications")
 @RestController
 @CrossOrigin(origins = {"http://localhost:5173","https://vikkischool-lms.vercel.app"})
 
